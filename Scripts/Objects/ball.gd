543982ba82b2a91d2c2030a7fb991a8d35662f4b
Kenneth Lauer <kenneth.j.lauer@protonmail.com>
2025-09-22 16:58:33 -0400

    Update ball.gd

--- Scripts/Objects/ball.gd
+++ Scripts/Objects/ball.gd
@@ -6,7 +6,8 @@ extends RigidBody3D
 
 var input_direction: Vector3 = Vector3.ZERO
 var forward_direction: Vector3 = Vector3.ZERO
-var angle_to_forward_dir: float = 0.0
+var forward_dir_angle: float = 0.0
+var prev_velocity: Vector3 = Vector3.ZERO
 @export var camera_speed: float = 0.5
 @onready var camera_anchor: Marker3D = $CameraAnchor
 @onready var base_rotator: Node3D = $H_Rotation
@@ -21,6 +22,7 @@ func _process(delta: float):
 	camera_follow(delta)
 	DebugOverlay.draw.add_vector(self, "linear_velocity", 1, 4, Color(1, 1, 1, 0.75))
 	DebugOverlay.draw.add_vector(self, "forward_direction", 1, 4, Color(1, 1, 0, 0.75))
+	DebugOverlay.draw.add_vector(self, "prev_velocity", 1, 4, Color(1, 0, 0, 0.75))
 
 
 func handle_directional_input():
@@ -32,7 +34,7 @@ func get_direction() -> bool:
 	var input: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	input_direction = Vector3(input.x, 0.0, input.y).normalized()
 	if input_direction != Vector3.ZERO:
-		forward_direction = input_direction.rotated(Vector3.UP, get_camera_yaw_angle_rad())
+		forward_direction = input_direction.rotated(Vector3.UP, get_camera_rotation().y)
 		return true
 	else: return false
 
@@ -40,16 +42,20 @@ func get_direction() -> bool:
 func move_ball():
 	if input_direction != Vector3.ZERO:
 		linear_velocity += Vector3(forward_direction.x, 0.0, forward_direction.z) * speed
-		angle_to_forward_dir = linear_velocity.signed_angle_to(forward_direction, Vector3.UP)
+		prev_velocity = linear_velocity.normalized()
+		#print(angle_to_forward_dir)
 
 
-func get_camera_yaw_angle_rad() -> float:
-	return base_rotator.global_transform.basis.get_euler().y
+func get_camera_rotation() -> Vector3:
+	return base_rotator.global_transform.basis.get_euler()
 
 
 func camera_follow(delta: float):
 	base_rotator.global_position = camera_anchor.get_global_transform_interpolated().orthonormalized().origin
 	
-	var theta: float = wrapf(atan2(angle_to_forward_dir, get_camera_yaw_angle_rad()), -PI, PI)
-	prints(angle_to_forward_dir, theta)
-	base_rotator.rotation.y += clamp(camera_speed * delta, 0, abs(theta)) * sign(theta)
+	forward_dir_angle = prev_velocity.signed_angle_to(forward_direction, Vector3.UP)
+	#var camera_angle_dif = angle_difference(forward_dir_angle, )
+	var theta: float = atan2(get_camera_rotation().y, forward_dir_angle)
+	prints(forward_dir_angle, get_camera_rotation(), theta)
+	#if forward_dir_angle > 0.5 or forward_dir_angle < -0.5:
+		#base_rotator.rotation.y += clamp(camera_speed * delta, 0, abs(theta)) * sign(theta)
